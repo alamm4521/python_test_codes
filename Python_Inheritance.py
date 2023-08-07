e043dc13e098ff583cd2eadd58506eb86cd57b24
hirok <alamm4521@gmail.com>
2023-08-07 19:30:21 +0600

    update

--- Python_Inheritance.py
+++ Python_Inheritance.py
@@ -68,3 +68,74 @@ x.printname()
 
 #Add the __init__() Function
 
+'''
+Add the __init__() Function
+So far we have created a child class that inherits the properties and methods from its parent.
+
+We want to add the __init__() function to the child class (instead of the pass keyword).
+'''
+
+#Note: The __init__() function is called automatically every time the class is being used to create a new object.
+
+'''
+Add the __init__() Function
+So far we have created a child class that inherits the properties and methods from its parent.
+
+We want to add the __init__() function to the child class (instead of the pass keyword).
+
+Note: The __init__() function is called automatically every time the class is being used to create a new object.
+
+Example
+Add the __init__() function to the Student class:
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    #add properties etc.
+When you add the __init__() function, the child class will no longer inherit the parent's __init__() function.
+
+Note: The child's __init__() function overrides the inheritance of the parent's __init__() function.
+
+To keep the inheritance of the parent's __init__() function, add a call to the parent's __init__() function:
+'''
+
+class Person008:
+     def __init__(self, fname, lname):
+          self.firstname = fname
+          self.lastname  = lname
+
+     def printname(self):
+          print(self.firstname, self.lastname)
+
+class Student_001:
+     def __init__(self, fname, lname):
+          Person008.__init__(self, fname, lname)
+
+x = Student("Michael", "Kelso")
+x.printname
+
+'''
+Now we have successfully added the __init__() function, 
+and kept the inheritance of the parent class, and we are ready to 
+add functionality in the __init__() function.
+'''
+
+#Example Add a year parameter, and pass the correct year when creating objects:
+
+class Person_009:
+     def __init__(self, fname, lname):
+          self.firstname = fname
+          self.lastname = lname
+
+     def printname(self):
+          print(self.firstname, self.lastname)
+     
+class Student002(Person_009):
+     def __init__(self, fname, lname, year):
+          super().__init__(fname, lname)
+          self.graduationyear = year
+
+print("What year did they graduate in That 70s Show?")
+print("In Season 5, Episode 25 (Celebration Day), their tassels show 78 (for Class of 1978).")
+x = Student002("Michael", "Kelso", "1978")  
+print(x.graduationyear)
+
