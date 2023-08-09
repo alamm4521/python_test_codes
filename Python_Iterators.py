fc5c5a1340e7c17485553cc4e1960f05aace7a00
hirok <alamm4521@gmail.com>
2023-08-09 11:34:21 +0600

    new file Python Iterators

---
+++ Python_Iterators.py
@@ -0,0 +1,46 @@
+
+#Python Iterators (with 70s show)
+
+'''
+Python Iterators
+An iterator is an object that contains a countable number of values.
+
+An iterator is an object that can be iterated upon, meaning that you can traverse through all the values.
+
+Technically, in Python, an iterator is an object which implements the iterator protocol, which consist of the methods __iter__() and __next__().
+'''
+
+'''
+Iterator vs Iterable
+Lists, tuples, dictionaries, and sets are all iterable objects. They are iterable containers which you can get an iterator from.
+
+All these objects have a iter() method which is used to get an iterator:
+'''
+
+mytuple = ("red", "bob", "pam", "kitty", "midiz")
+
+miit = iter(mytuple)
+
+print(next(miit))
+print(next(miit))
+print(next(miit))
+print(next(miit))
+print(next(miit))
+
+#Even strings are iterable objects, and can return an iterator:
+
+#Example
+#Strings are also iterable objects, containing a sequence of characters:
+print("#Strings are also iterable objects, containing a sequence of characters:")
+mystr = "bananan"
+
+miit = iter(mystr)
+
+print(next(miit))
+print(next(miit))
+print(next(miit))
+print(next(miit))
+print(next(miit))
+print(next(miit))
+print(next(miit))
+
