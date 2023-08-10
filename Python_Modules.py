9fd669acee4b6821db9f99a3e18ae2fcd1b423be
hirok <alamm4521@gmail.com>
2023-08-10 20:46:49 +0600

    update

---
+++ Python_Modules.py
@@ -0,0 +1,17 @@
+import 
+
+#Python Modules
+
+'''
+What is a Module?
+Consider a module to be the same as a code library.
+
+A file containing a set of functions you want to include in your application.
+'''
+
+#Create a Module
+#To create a module just save the code you want in a file with the file extension .py:
+
+
+
+mymodule.greetings("kalso")
